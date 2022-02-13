5bb330ab5b93197eff1a94a6503b85f3159d41a3
Margo Parkala <mparkala@tktk.ee>
2022-02-13 22:01:26 +0200

    Control Flow

---
+++ DrivingLicense/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace DrivingLicense
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja sünniaastat
+            //programm arvutab kasutaja vanust
+            //kui kasutaja on noorem kui 18, siis programm kuvab konsoolis
+            //"Oled liiga noor, et juhilube saada"
+            //kui kasutaja on vanem kui 18, siis programm kuvab konsoolis
+            //"oled piisavalt vana, et juhilube saada"
+            //kui kasutaja on 18, siis programm kuvab konsoolis
+            //"Palju õnne, nüüd on teil võimalik juhilube taotleda"
+
+            Console.WriteLine("Palun sisestage enda sünniaasta.");
+            int yearÓfBirth = Convert.ToInt32(Console.ReadLine());
+            int userAge = 2021 - yearÓfBirth;
+
+            if (userAge < 18)
+            {
+                Console.WriteLine("Oled liiga noor, et juhilube saada!");
+            }
+
+            else if (userAge > 18)
+            {
+                Console.WriteLine("Oled piisavalt vana, et juhilube saada!");
+            }
+            
+            else
+            {
+                Console.WriteLine("Palju õnne, nüüd on teil võimalik juhilube taotleda!");
+            }
+            
+                
+
+            
+
+            
+          
+            
+        }
+    }
+}
---
+++ IstagramAgeCheck/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace IstagramAgeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutaja vanust;
+            //programm kontrollib kas kasutaja on vanem kui 13 või 13 aastat vana;
+            //kui kasutaja on 13 aastat vana või vanem, siis ta võib instagrami kasutada;
+            //kui kasutaja on noorem kui 13, siis on ta liiga noor;
+
+            Console.WriteLine("Palun sisesta oma vanus:");
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            if (userAge > 13)
+            {
+                Console.WriteLine("Oled piisavalt vana, et Instagrami kontot luua.");
+
+            }
+
+            else if (userAge < 13)
+            {
+                Console.WriteLine("Oled liiga noor, et Instagrami kontot luua");
+
+            }
+            else
+            {
+                Console.WriteLine("Oledki 13, nüüd on sul võimalik luua endale Instagrami konto.");
+            }
+            Console.WriteLine("Kena päeva!");
+           
+
+
+
+
+        }
+    }
+}
